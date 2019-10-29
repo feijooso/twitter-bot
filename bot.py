a99b94efd7c9a631427a9ca86e46cbedc53edfaf
sofi <sofeijoo@gmail.com>
2019-10-29 12:38:03 -0300

    push inicial

---
+++ bot.py
@@ -0,0 +1,12 @@
+import tweepy
+print('hello, im bot')
+
+CONSUMER_KEY = ""  
+CONSUMER_SECRET = ""  
+ACCESS_KEY = ""  
+ACCESS_SECRET = ""
+
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
