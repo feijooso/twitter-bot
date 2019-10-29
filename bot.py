2eb831db4df2e5a2edca931a143ff7ac3ded56cd
sofi <sofeijoo@gmail.com>
2019-10-29 14:28:23 -0300

    added gitignore for secrets and updated the bot functions

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import tweepy
-print('hello, im bot')
+print('hello, im bot!')
 
-CONSUMER_KEY = ""  
-CONSUMER_SECRET = ""  
-ACCESS_KEY = ""  
-ACCESS_SECRET = ""
-
-
+from secrets import *
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
+
+mentions = api.mentions_timeline()
+
+for mention in mentions:
+	print(str(mention.id) + ' ' + mention.text)
