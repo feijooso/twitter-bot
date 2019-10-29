72408ba37702aca2bfbb24f78b0d555d08c5b62e
sofi <sofeijoo@gmail.com>
2019-10-29 15:33:59 -0300

    now responding

--- bot.py
+++ bot.py
@@ -1,12 +1,19 @@
 import tweepy
 print('hello, im bot!')
 
-from secrets import *
+from secrets import * #takes the keys from the secrets.py file
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-mentions = api.mentions_timeline()
+
+mentions = api.mentions_timeline(
+                        # last_seen_id,
+                        tweet_mode='extended')
 
 for mention in mentions:
-	print(str(mention.id) + ' ' + mention.text)
+	#print(str(mention.id) + ' ' + mention.text)
+	if 'hola' in mention.full_text.lower():
+		api.update_status('@' + mention.user.screen_name + '#HelloWorld back to you!', mention.id)
+
+
